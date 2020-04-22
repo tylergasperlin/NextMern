017c4d0873d63de9efbb51bd60a2f691f24cfee2
Tyler Gasperlin <gasperlint@gmail.com>
2020-04-22 06:31:44 -0700

    Finish ui for activate accoun

--- client-app/pages/auth/activate/[id].js
+++ client-app/pages/auth/activate/[id].js
@@ -23,8 +23,34 @@ const ActivateAccount = ({ router }) => {
             const { name } = jwt.decode(token);
             setState({ ...state, name, token });
         }
-    });
-    return <Layout>{JSON.stringify(state)}</Layout>;
+    },[router]);
+
+    const clickSubmit = async (e) => {
+        e.preventDefault();
+        setState({...state, buttonText: 'Activating'})
+        try {
+            const response = await axios.post(`${API}/register/activate`, {token})
+            console.log('account activate response', response)
+            setState({...state,name:'', token: '', buttontext:'Activated', success: response.data.message})
+        } catch(error){
+            setState({...state, buttontext:'Activate Account', error: error.response.data.error})
+        }
+    };
+    return (
+        <Layout>
+            <div className='row'>
+                <div className='col-md-6 offset-md-3'>
+                    <h1>Good day {name}, React to activate your account?</h1>
+                    <br />
+                    {success && showSuccessMessage(success)}
+                    {error && showErrorMessage(error)}
+                    <button onClick={clickSubmit} className='btn btn-outline-warning btn-block'>
+                        {buttontext}
+                    </button>
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default withRouter(ActivateAccount);
