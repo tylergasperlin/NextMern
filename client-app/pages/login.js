9008d3314b0b206daf16d9ea09d3afc8ef0cb112
Tyler Gasperlin <gasperlint@gmail.com>
2020-04-28 23:21:31 -0700

    Implement login page

--- client-app/pages/login.js
+++ client-app/pages/login.js
@@ -1,7 +1,88 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+import {showSuccessMessage, showErrorMessage} from '../helpers/alerts';
+import { API } from '../config';
+import Link from 'next/link';
+import Router from 'next/router'
 
 const Login = () => {
-    return <Layout>Login page</Layout>
-}
+    const [state, setState] = useState({
+        email: 'moseu@gmail.com',
+        password: 'asfasfw',
+        error: '',
+        success: '',
+        buttontext: 'Login',
+    });
+
+    const { email, password, error, success, buttontext } = state;
+
+    const handleChange = (name) => (e) => {
+        
+        setState({
+            ...state,
+            [name]: e.target.value,
+            error: '',
+            success: '',
+            buttontext: 'Login',
+        });
+    };
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setState({...state, buttontext: 'Logging in'})
+        try {
+            const response = await axios.post(`${API}/login`, {
+                email, password
+            })
+            console.log(response)
+        } catch(error) {
+            console.log(error)
+            setState({...state, buttonText: 'Login', error: error.response.data.error})
+        }
+    };
+
+    const loginForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <input
+                        value={email}
+                        onChange={handleChange('email')}
+                        type='email'
+                        className='form-control'
+                        placeholder='Type your email'
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        value={password}
+                        onChange={handleChange('password')}
+                        type='password'
+                        className='form-control'
+                        placeholder='Type your password'
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <button className='btn btn-outline-dark'>{buttontext}</button>
+                </div>
+            </form>
+        );
+    };
+
+    return (
+        <Layout>
+            <div className='col-md-6 offset-md-3'>
+                <h1>Login</h1>
+                <br />
+                {success && showSuccessMessage(success)}
+                {error && showErrorMessage(error)}
+                {loginForm()}
+            </div>
+        </Layout>
+    );
+};
 
 export default Login;
