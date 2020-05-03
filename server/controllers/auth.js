189a0d3a29a49211f010b5e5d55e8ac253125d6d
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-03 07:38:07 -0700

    Add admin middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -117,3 +117,37 @@ exports.login = (req, res) => {
 
 
 exports.requireSignin = expressJwt({secret: process.env.JWT_SECRET})
+
+exports.authMiddleware = (req, res, next) => {
+    const authUserId = req.user._id
+    User.findOne({_id: authUserId}).exec((err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: "User not found"
+            })
+        }
+        req.profile = user
+        next() 
+    })
+}
+
+
+exports.adminMiddleware = (req, res, next) => {
+    const adminUserId = req.user._id
+    User.findOne({_id: adminUserId}).exec((err, user) => {
+        if(err || !user) {
+            return res.status(400).json({
+                error: "User not found"
+            })
+        }
+
+        if(user.role !== 'admin') {
+            return res.status(400).json({
+                error: "This is an Admin resource. Access denied"
+            }) 
+        }
+
+        req.profile = user
+        next() 
+    })
+}
