ca5102d363ad5db1a8876ba766cf99138fe6f5c4
Tyler Gasperlin <gasperlint@gmail.com>
2020-06-23 23:43:53 -0700

    Server side rendering example

--- client-app/pages/user/index.js
+++ client-app/pages/user/index.js
@@ -1,7 +1,19 @@
-import Layout from '../../components/Layout'
+import Layout from '../../components/Layout';
+import axios from 'axios';
 
-const User = () => {
-    return <Layout>Hello user</Layout>
+
+const User = ({todos}) => {
+    return <Layout>{JSON.stringify(todos)}</Layout>
+}
+
+// getInitialProps is a method specific to Next that allows for server side rendering
+// Server side rendering good for SEO because the page is rendered with data
+User.getInitialProps = async () => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
+    console.log('SERVER RENDERED', response)
+    return {
+        todos: response.data
+    }
 }
 
 export default User;
